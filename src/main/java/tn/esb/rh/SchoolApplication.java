5e4b62cc34e3513586a9668a6c0b53865b735a1e
Chahir77 <chahir.elbahri@gmail.com>
2025-05-19 15:30:09 +0100

    second commit

--- src/main/java/tn/esb/rh/SchoolApplication.java
+++ src/main/java/tn/esb/rh/SchoolApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SchoolApplication {
 
 	public static void main(String[] args) {
+		//test
 		SpringApplication.run(SchoolApplication.class, args);
 	}
 
